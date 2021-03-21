60569c81dfc972e9029e28a20ad3adb962e41150
hayata yamada <hayata@hayata-MacBook.local>
2021-03-21 19:41:14 +0900

    ランキング機能追加完了” git commit -m ランキング機能追加完了”

--- www/html/index.php
+++ www/html/index.php
@@ -14,5 +14,7 @@ $db = get_db_connect();
 $user = get_login_user($db);
 $token = get_csrf_token();
 $items = get_open_items($db);
+$ranking = get_ranking($db);
+$rank_no = 1;
 
 include_once VIEW_PATH . 'index_view.php';
